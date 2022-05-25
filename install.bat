de1a66e08161828f2982a8bd129befe0f291a94e
zaqo <96392276+zaqoQLF@users.noreply.github.com>
2022-05-25 23:22:20 +0200

    Delete install.bat

--- install.bat
+++
@@ -1,8 +0,0 @@
-echo "Installing Dependencies..."
-pip install os
-pip install time
-pip install socket
-pip install cryptography
-pip install discord_webhook
-pip install colorama
-pip install binascii
